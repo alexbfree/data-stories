8b43b80520382ebb23920915ef2a92dab7364b14
francoisquellec <francois.quellec@gmail.com>
2022-01-28 17:17:18 +0100

    fix css and fonts for vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,22 +60,10 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['@/assets/styles/variables.sass'],
+    treeShake: true,
     optionsPath: 'vuetify.options.js',
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
+    defaultAssets: false
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
