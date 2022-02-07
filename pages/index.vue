578fd9af0be1593cbdc4f49de6accc351aa23aa2
francoisquellec <francois.quellec@gmail.com>
2022-02-07 16:56:55 +0100

    fetch json for mkn

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,17 @@
 </template>
 
 <script>
-import manifest from '@/manifest/mkn.json'
 export default {
   name: 'IndexPage',
   data() {
     return {
-      manifest
+      manifest: {}
     };
-  }
+  },
+  created() {
+        fetch('/manifest/mkn.json')
+          .then(response => response.json())
+            .then(data => (this.manifest = data))
+    }
 }
 </script>
